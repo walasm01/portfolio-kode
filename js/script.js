5cc969e862ae92f455f4e1562aa04c101304ba31
Walter Liao Asmussen <walasm01@iba.dk>
2025-09-24 14:05:07 +0200

    Tilføjet anchors til projekt-titlerne

--- js/script.js
+++ js/script.js
@@ -45,6 +45,7 @@
 class Project {
     constructor({
         title, 
+        link, 
         undertitle, 
         mainImg, 
         mainAltText, 
@@ -57,6 +58,7 @@ class Project {
         icons
     }) {
         this.title = title;
+        this.link = link;
         this.undertitle = undertitle;
         this.mainImg = mainImg;
         this.mainAltText = mainAltText;
@@ -73,7 +75,7 @@ class Project {
         return `
         <section class="project">
             <div class="project-title">
-                <h3>${this.title}</h3>
+                <h3><a href="${this.link}" target="_blank">${this.title}</a></h3>
                 <p class="undertitle">${this.undertitle}</p>
             </div>
             <div class="project-content">
@@ -97,7 +99,7 @@ class Project {
                     </div>
                 </div>
                 <div class="icons">
-                    <p class="icons-text">Disse programmer har jeg brugt i projektet:</p>
+                    <p class="icons-text">Disse værktøjer har jeg brugt i projektet:</p>
                     <div class="icon-container">
                         ${this.icons.map(icon => `<img class="icon" src="${icon}" alt="">`).join('')}
                     </div>
@@ -111,6 +113,7 @@ class Project {
 
 const cameleonCreative = new Project({
     title: "CAMELEON CREATIVE",
+    link: "https://www.figma.com/proto/sbqT4mJCSFvGOB05ujexYz/Branding-Projekt?page-id=724%3A1085&node-id=887-1573&p=f&viewport=479%2C820%2C0.05&t=aN9T4XDCHno2LTir-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=887%3A1573",
     undertitle: "IBA Multimediedesigner</br>2. Semester, 1. Projekt",
     mainImg: "img/cc-phone-and-pc-mockup.png",
     mainAltText: "Cameleon Creative hjemmeside på PC og mobil",
@@ -130,21 +133,21 @@ const cameleonCreative = new Project({
 
 const datasikkerhed = new Project({
     title: "SPILLET OM DATASIKKERHED",
+    link: "https://walterliaoasmussen.dk/datasikkerhed",
     undertitle: "IBA Multimediedesigner</br>2. Semester, 2. Projekt",
     mainImg: "img/datasikkerhed-laptop-and pc-mockup-v3.png",
-    mainAltText: "Cameleon Creative hjemmeside på PC og mobil",
-    descriptionTitle: "Et Multimediebureau",
+    mainAltText: "Spillet om datasikkerhed på PC og mobil",
+    descriptionTitle: "Branching Scenarier",
     description: 
-        "Dette var et gruppeprojekt, hvor formålet var at lave en brandstrategi og en visuel kampagne for vores eget nystartede multimediebureau. I projektet har vi arbejdet iterativt med bl.a.:",
+        "Dette var et individuelt projekt, hvor formålet var at formidle information om datasikkerhed gennem branching-scenarier. Dette skulle gøres på en kreativ og informativ måde med HTML, CSS og JavaScript. Til dette har jeg arbejdet med bl.a.:",
     descriptionList: `
-        <li>Udarbejdelse af visuelle elementer for brandet, såsom logo, lavet i Adobe Illustrator</li>
-        <li>En high-fidelity visuel prototype af et responsivt website på figma</li>
-        <li>En designmanual for vores brand, lavet i Adobe InDesign</li>`,
-    secImgs: ["img/cc-visitkort.png", "img/cc-designguide-mockup.png"],
-    resultTitle: "Min rolle",
+        <li>Brainstorming og sketching af branching scenarier</li>
+        <li>Iterativ og responsiv kodning med HTML, CSS og JavaScript</li>`,
+    secImgs: ["img/datasikkerhed-branching.jpg", "img/datasikkerhed-phone-mockup.png"],
+    resultTitle: "Et responsivt resultat",
     result: 
-        "Resultatet blev et farverigt og energisk brand for vores multimediebureau, som vi kalder for Cameleon Creative. Hele brandet er baseret på kamæleonen, da den er et symbol på kreativitet. Jeg havde et større ansvar i at udvikle brandets visuelle identitet, hvilket indebar farvevalg, fonte, udarbejdelse af logo mm. Alle disse elementer skulle jeg forbinde til en konkret og konsistent visuel brandidentitet, som kommer til udtryk i figma prototypen.",
-    icons: ["img/figma-sticker-v2.png", "img/illustrator-sticker-v2.png", "img/indesign-sticker-v2.png", "img/photoshop-sticker.png", "img/aftereffect-sticker.png", "img/wordpress-sticker.png"]
+        "Jeg endte med at lave et spil om datasikkerhed, hvor man ville blive præsenteret for forskellige “Smishing” (SMS phishing) scenarier med forskellige svarmuligheder. Ud fra hvad man svarede kunne man få 6 forskellige slutninger.",
+    icons: ["img/figma-sticker-v2.png", "img/html5-sticker.png", "img/css3-sticker.png", "img/js-sticker.png"]
 });
 
 document.addEventListener("DOMContentLoaded", () => {
