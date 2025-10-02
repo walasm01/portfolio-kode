002a336fe02dcd077b350747cdf8609492ac1519
Walter Liao Asmussen <walasm01@iba.dk>
2025-10-02 14:10:05 +0200

    Har tilføjet nogle illustrationer som CTA'er

--- src/casesPage.js
+++ src/casesPage.js
@@ -5,7 +5,7 @@ import Project from "./projectClass.js";
 const greyWolf = new Project({
     projectId: "project__greywolf",
     title: "GREY WOLF",
-    link: "",
+    link: "https://www.figma.com/proto/H00JvqqRyUImhthIwhwUkb/Grey-Wolf?page-id=29%3A140&node-id=161-265&p=f&viewport=-615%2C222%2C0.08&t=PBuzx0SsAxs2FT1n-1&scaling=scale-down&content-scaling=fixed",
     undertitle: "IBA Multimediedesigner</br>1. Semester, 1. Projekt",
     mainImg: "img/gw-double-phone-mockup.png",
     mainAltText: "Grey Wolf hjemmeside vist på to telefoner",
@@ -21,7 +21,7 @@ const greyWolf = new Project({
     result: 
         "Grey Wolf har et tæt samarbejde med Roskilde Festival, hvor de indsamler de efterladte festivaltelte, og bruger materialet til at lave langtidsholdbart udendørstøj. I gruppen havde vi et tæt samarbejde, men hvor vi hver især havde vores egne fokusområder. Jeg havde et stort ansvar i udviklingen af Grey Wolfs brandidentitet og deres visuelle udtryk på figma-prototypen.",
     icons: ["img/figma-sticker-v2.png", "img/illustrator-sticker-v2.png"],
-    stickerImg: "img/greywolf-character-sticker.png",
+    stickerImg: "img/gw-sticker-text2.png",
 });
 
 const ibaVidensbank = new Project({
@@ -70,7 +70,7 @@ const godsetUng = new Project({
 const cameleonCreative = new Project({
     projectId: "project__cameleon",
     title: "CAMELEON CREATIVE",
-    link: "https://www.figma.com/proto/sbqT4mJCSFvGOB05ujexYz/Branding-Projekt?page-id=724%3A1085&node-id=887-1573&p=f&viewport=479%2C820%2C0.05&t=aN9T4XDCHno2LTir-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=887%3A1573",
+    link: "https://www.figma.com/proto/sbqT4mJCSFvGOB05ujexYz/Branding-Projekt?page-id=724%3A1085&node-id=887-1573&p=f&viewport=479%2C820%2C0.05&t=yCMFLZnLU3YHy1Sz-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=887%3A1573",
     undertitle: "IBA Multimediedesigner</br>2. Semester, 1. Projekt",
     mainImg: "img/cc-phone-and-pc-mockup.png",
     mainAltText: "Cameleon Creative hjemmeside på PC og mobil",
@@ -86,7 +86,7 @@ const cameleonCreative = new Project({
     result: 
         "Resultatet blev et farverigt og energisk brand for vores multimediebureau, som vi kalder for Cameleon Creative. Jeg havde et større ansvar i at udvikle brandets visuelle identitet, hvilket indebar farvevalg, fonte, udarbejdelse af logo mm. Alle disse elementer skulle jeg forbinde til en konkret og konsistent visuel brandidentitet, som kommer til udtryk i figma prototypen.",
     icons: ["img/figma-sticker-v2.png", "img/illustrator-sticker-v2.png", "img/indesign-sticker-v2.png", "img/photoshop-sticker.png", "img/aftereffect-sticker.png", "img/wordpress-sticker.png"],
-    stickerImg: "img/proaximon-sticker.png",
+    stickerImg: "img/cc-sticker-text.png",
 });
 
 const datasikkerhed = new Project({
